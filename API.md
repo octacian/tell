0914819f5eec5889ab72708280c034868404993c
octacian <enduffy2014@outlook.com>
2017-04-04 08:00:47 -0700

    Add `tell.add` and `tell.remove` API
    Documented in API.md.

---
+++ API.md
@@ -0,0 +1,16 @@
+Tell API
+========
+Tell has a very simple API composed of two functions for use by other mods.
+
+`tell.add(name, from, msg)`
+
+* Adds a message to be shown when the recipient is active
+* `name`: Name of recipient (not `userdata`)
+* `from`: Name of sender
+* `msg`: Message
+
+`tell.remove(name, id)`
+
+* Removes a message
+* `name`: Name of recipient (not `userdata`)
+* `id`: Numerical ID of the message (returned by `tell.add`)
--- init.lua
+++ init.lua
@@ -25,3 +25,27 @@ load()
 
 -- [register] On shutdown
 minetest.register_on_shutdown(save)
+
+-- [function] Add item
+function tell.add(name, from, msg)
+  if not list[name] then
+    list[name] = {}
+  end
+
+  list[name][#list[name] + 1] = {
+    from = from,
+    msg  = msg,
+    time = os.date("%I:%M %p on %B %d, %Y"),
+  }
+
+  minetest.log("action", dump(list))
+
+  return #list[name] + 1
+end
+
+-- [function] Remove item
+function tell.remove(name, id)
+  if list[name] and list[name][id] then
+    list[name][id] = nil
+  end
+end
