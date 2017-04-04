2a84d977ee0121cc1f2d0a534120743412ccddb9
octacian <enduffy2014@outlook.com>
2017-04-04 07:57:33 -0700

    Add `save()` and `load()`
    Data will be stored in `<world>/tell.txt`

--- init.lua
+++ init.lua
@@ -1,1 +1,27 @@
 -- tell/init.lua
+
+tell = {}
+
+local list = {}
+
+-- [local function] Load
+local function load()
+  local res = io.open(minetest.get_worldpath().."/tell.txt", "r")
+  if res then
+    res = minetest.deserialize(res:read("*all"))
+    if type(res) == "table" then
+      list = res
+    end
+  end
+end
+
+-- [local function] Save
+local function save()
+  io.open(minetest.get_worldpath().."/tell.txt", "w"):write(minetest.serialize(list))
+end
+
+-- Load
+load()
+
+-- [register] On shutdown
+minetest.register_on_shutdown(save)
